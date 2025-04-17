62b77a568a7b54141a884785c15f8b8d498cc209
Siempreflaco <113026366+Siempreflaco@users.noreply.github.com>
2025-04-17 07:52:48 -0400

    Load 3D Mesh From Outputs Node

--- mesh_nodes.py
+++ mesh_nodes.py
@@ -3,20 +3,25 @@ import trimesh as Trimesh
 
 mesh_extensions = ['.glb', '.gltf', '.obj', '.ply', '.stl']
 output_dir = folder_paths.get_output_directory()
-all_files, _ = folder_paths.recursive_search(output_dir)
-file_list = folder_paths.filter_files_extensions(all_files, mesh_extensions)
-files = []
+
+def get_mesh_files():
+    all_files, _ = folder_paths.recursive_search(output_dir)
+    return folder_paths.filter_files_extensions(all_files, mesh_extensions)
+
+def get_afilepath(path):
+    return folder_paths.get_annotated_filepath(path, output_dir)
+
+def load_trimesh(mesh_file):
+    return Trimesh.load(mesh_file, force="mesh")
 
 class Load3DMesh:
     @classmethod
     def INPUT_TYPES(s):
-        for file in file_list:
-            rel_path = file
-            files.append(rel_path)
+        files = sorted(get_mesh_files())
 
         return {
             "required": {
-                "mesh": (sorted(files),), 
+                "mesh": (files,), 
             }
         }
     
@@ -25,10 +30,13 @@ class Load3DMesh:
     
     FUNCTION = "load"
     CATEGORY = "NCNodes/3D"
-    DESCRIPTION = "List 3D files in the output folder and loads selected file as trimesh."
-    EXPERIMENTAL = True # Need to find a way to refresh the list of files if anything changes in the output directory
+    DESCRIPTION = """
+List 3D files in the output folder and loads selected file as trimesh.
+When new files are added or removed from the output directory
+press R to refresh the node definitions.
+"""
 
     def load(self, mesh):
-        mesh_file = folder_paths.get_annotated_filepath(mesh, output_dir)
-        trimesh = Trimesh.load(mesh_file, force="mesh")
+        mesh_file = get_afilepath(mesh)
+        trimesh = load_trimesh(mesh_file)
         return (trimesh, mesh_file,)
